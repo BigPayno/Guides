f08d9e742073ca27fca73fe8559d2e10666a4ec0
BigPayno <1361098889@qq.com>
2020-06-05 14:28:51 +0800

    事务大概了解下，索然无味 离开了

--- springs/transaction/src/main/java/com/payno/transaction/TransactionApplication.java
+++ springs/transaction/src/main/java/com/payno/transaction/TransactionApplication.java
@@ -1,15 +1,33 @@
 package com.payno.transaction;
 
+import org.springframework.beans.BeansException;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.transaction.TransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
-public class TransactionApplication {
+public class TransactionApplication implements ApplicationRunner, ApplicationContextAware {
+
+    ApplicationContext context;
 
     public static void main(String[] args) {
         SpringApplication.run(TransactionApplication.class, args);
     }
 
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.err.println(context.getBean(TransactionManager.class).getClass());
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.context = applicationContext;
+    }
 }
