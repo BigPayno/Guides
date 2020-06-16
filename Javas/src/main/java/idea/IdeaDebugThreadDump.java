766e5a42d07f33c641938bf991f014bc0e936e33
BigPayno <1361098889@qq.com>
2020-06-16 16:27:41 +0800

    IDEA Plugins Shiro framework

---
+++ Javas/src/main/java/idea/IdeaDebugThreadDump.java
@@ -0,0 +1,21 @@
+package idea;
+
+/**
+ * @author payno
+ * @date 2020/6/16 14:51
+ * @description
+ */
+public class IdeaDebugThreadDump {
+    /**
+     *  debug时，有个相机一样的东西可以打印线程栈状况
+     *      https://www.cnblogs.com/xiaowenshu/p/10205238.html
+     *
+     *
+     *  SequenceDiagram for IntelliJ IDEA
+     *      调用时序图
+     *  Diagram 生成类图
+     *  Jprofiler
+     *      内存监控
+     *  Memory View Tool Window
+     */
+}
