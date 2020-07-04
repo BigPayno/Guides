9dc4c4ad8f3af1d0ea15284efa916df9cc0647d0
BigPayno <1361098889@qq.com>
2020-07-04 12:56:09 +0800

    Easy Java Doc

---
+++ algorithms/src/main/java/test/EasyDocTest.java
@@ -0,0 +1,22 @@
+package test;
+
+/**
+ * 简单的文档测试
+ *
+ * @author payno
+ * @date 2020/07/04
+ *
+ * 真的好用
+ */
+public class EasyDocTest {
+
+    /**
+     * 简单的方法文档测试
+     *
+     * @param source 源
+     * @return int* @throws Exception 异常
+     */
+    private int easyMethodDocTest(String source) throws Exception{
+        return 1;
+    }
+}
