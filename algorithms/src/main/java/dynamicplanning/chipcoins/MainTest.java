6bb911a277bc7cc34c458b7ceb8d4e15b6fd1a3b
BigPayno <1361098889@qq.com>
2020-07-04 18:58:43 +0800

    FibSeq ChipCoins

---
+++ algorithms/src/main/java/dynamicplanning/chipcoins/MainTest.java
@@ -0,0 +1,10 @@
+package dynamicplanning.chipcoins;
+
+public class MainTest {
+    public static void main(String[] args) {
+        ChipCoins chipCoins = new BaseChipCoins();
+        System.out.println(
+                chipCoins.coinsChange(new int[]{1,2,5,10},25)
+        );
+    }
+}
