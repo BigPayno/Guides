e8b17e961991c27534dcbf4c452532ee6dc0617f
zhaolei22 <zhaolei22@changingedu.com>
2020-07-22 18:18:42 +0800

    BF update GC todo TransactionTemplate todo

---
+++ Javas/src/main/java/jvm/JvmGC.java
@@ -0,0 +1,10 @@
+package jvm;
+
+/**
+ * jvm gc
+ *
+ * @author zhaolei22
+ * @date 2020/07/22
+ */
+public class JvmGC {
+}
---
+++ springs/transaction/framework/src/main/java/com/payno/transaction/template/TransactionTemplateGuide.java
@@ -0,0 +1,10 @@
+package com.payno.transaction.template;
+
+/**
+ * 事务模板指导
+ *
+ * @author zhaolei22
+ * @date 2020/07/22
+ */
+public class TransactionTemplateGuide {
+}
